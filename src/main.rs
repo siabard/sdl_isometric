4701368ba2d8e590274cf1fa521d2c67c9afcf99
Yeonho Jang <siabard@gmail.com>
2020-03-24 13:01:04 +0900

    State 전환

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,72 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 enum StateResult {
+    Push(&'static str),
+    Trans(&'static str),
+    Pop,
     Default,
 }
 
 trait States {
-    fn update(&mut self) -> StateResult;
+    fn update(&mut self, event: &sdl2::event::Event) -> StateResult;
     fn render(&self, canvas: &mut WindowCanvas) -> StateResult;
+}
+
+struct InitState<'a> {
+    //font: sdl2::ttf::Font<'a, 'b>,
+    //surface: sdl2::surface::Surface<'a>,
+    texture: Texture<'a>,
+}
+
+impl<'a> InitState<'a> {
+    fn new(
+        font_context: &'a sdl2::ttf::Sdl2TtfContext,
+        texture_creator: &'a TextureCreator<WindowContext>,
+    ) -> InitState<'a> {
+        let font = font_context
+            .load_font(Path::new("resources/hackr.ttf"), 36)
+            .unwrap();
+
+        let surface = font
+            .render("Init State")
+            .blended(Color::RGBA(255, 0, 0, 255))
+            .unwrap();
+
+        let texture = texture_creator
+            .create_texture_from_surface(&surface)
+            .unwrap();
+        InitState { texture }
+    }
+}
+
+impl<'a> States for InitState<'a> {
+    fn update(&mut self, event: &sdl2::event::Event) -> StateResult {
+        match event {
+            Event::KeyDown {
+                keycode: Some(Keycode::Q),
+                ..
+            } => StateResult::Push("game"),
+            _ => StateResult::Default,
+        }
+    }
+
+    fn render(&self, canvas: &mut WindowCanvas) -> StateResult {
+        // 화면에 텍스트를 출력하기
+
+        canvas
+            .copy_ex(
+                &self.texture,
+                None,
+                Some(Rect::new(0, 0, 400, 300)),
+                0.,
+                Some(Point::new(0, 0)),
+                false,
+                false,
+            )
+            .unwrap();
+
+        StateResult::Default
+    }
 }
 
 struct GameState<'a> {
@@ -45,8 +105,14 @@ impl<'a> GameState<'a> {
 }
 
 impl<'a> States for GameState<'a> {
-    fn update(&mut self) -> StateResult {
-        StateResult::Default
+    fn update(&mut self, event: &sdl2::event::Event) -> StateResult {
+        match event {
+            Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => StateResult::Pop,
+            _ => StateResult::Default,
+        }
     }
 
     fn render(&self, canvas: &mut WindowCanvas) -> StateResult {
@@ -86,6 +152,7 @@ fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().expect("ERROR on SDL CONTEXT");
     let video_subsystem = sdl_context.video().expect("ERROR on Video_subsystem");
     let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+    let font_context = sdl2::ttf::init().unwrap();
     let window = video_subsystem
         .window("isometric rust-sdl2 demo", 800, 600)
         .position_centered()
@@ -99,22 +166,45 @@ fn main() -> Result<(), String> {
     let mut event_pump = sdl_context.event_pump().expect("ERROR on event_pump");
     let mut i = 0;
 
+    let mut states: Vec<Box<dyn States>> = vec![];
+    states.push(Box::new(InitState::new(&font_context, &texture_creator)));
+    /*
     let mut game_state = GameState::new();
     game_state.add_sprite(
         &texture_creator,
         "image".to_string(),
         "resources/image.png".to_string(),
     );
+    */
     'running: loop {
         i = (i + 1) % 255;
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
+                Event::Quit { .. } => break 'running,
+                _ => {
+                    // 가장 상단의 sates에 대한 처리
+                    if let Some(state) = states.last_mut() {
+                        match state.update(&event) {
+                            StateResult::Push(s) => {
+                                println!("push check");
+                                if s == "game" {
+                                    println!("game check");
+                                    let mut game_state = GameState::new();
+                                    game_state.add_sprite(
+                                        &texture_creator,
+                                        "image".to_string(),
+                                        "resources/image.png".to_string(),
+                                    );
+                                    states.push(Box::new(game_state));
+                                }
+                            }
+                            StateResult::Pop => {
+                                states.pop().unwrap();
+                            }
+                            _ => (),
+                        }
+                    }
+                }
             }
         }
         // The rest of the game loop goes here...
@@ -125,8 +215,10 @@ fn main() -> Result<(), String> {
 
         //draw(&mut canvas, Color::RGB(i, 64, 255 - i), Some(&texture));
         canvas.clear();
-        game_state.update();
-        game_state.render(&mut canvas);
+        if let Some(state) = states.last_mut() {
+            state.render(&mut canvas);
+        }
+
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
