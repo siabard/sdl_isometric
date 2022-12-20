58b0b1de66ce782de09fbb9b3a3b18bb89045445
yhjang <siabard@gmail.com>
2022-12-20 22:54:53 +0900

    아스키 랜더러 작성

--- src/ascii/mod.rs
+++ src/ascii/mod.rs
@@ -3,18 +3,53 @@
 const ASCII_TEXTURE: &'static str = " .:-+*=%@#";
 
 /// 아스키 코드로 랜더링
+#[derive(Clone, Debug)]
 pub struct AsciiImage {
-    width: u32,
-    height: u32,
-    ascii_image: String,
+    pub width: u32,
+    pub height: u32,
+    pub ascii_image: String,
 }
 
 impl AsciiImage {
-    pub fn new(image: &image::DynamicImage) -> Self {
+    /// 가로 w픽셀, 세로 h 픽셀만큼을 한 아스키 코드로 만든다.
+
+    pub fn new(image: &image::DynamicImage, w: u32, h: u32) -> Self {
+        let buffer = image.to_luma8();
+        let mut mozaic: Vec<u32> = vec![];
+        let mut ascii_image: String = "".to_owned();
+        let width = buffer.width() / w;
+        let height = buffer.height() / h;
+
+        mozaic.resize_with(width as usize * height as usize, Default::default);
+
+        for y in 0..(height * h) {
+            for x in 0..(width * w) {
+                // 픽셀값을 평균으로 환산
+                let pixel = buffer.get_pixel(x, y).0[0];
+                let x_ = x / w;
+                let y_ = y / h;
+
+                let r = mozaic[(y_ * width + x_) as usize] + (pixel as u32);
+                mozaic[(y_ * width + x_) as usize] = r;
+            }
+        }
+
+        // 일정 구간의 값을 합산(wxh)하여 0~100 까지의 값(n)으로 하고
+        for y in 0..height {
+            for x in 0..width {
+                let n = mozaic[(y * width + x) as usize] * 100 / ((w * h * 255) as u32);
+                let p = (n as usize * ASCII_TEXTURE.len() / 100) as usize;
+                ascii_image = ascii_image + ASCII_TEXTURE.get(p..p + 1).unwrap();
+            }
+        }
+
+        // 이를 ASCII_TEXTURE와의 길이(l)에 대한 비례식으로 값을 구한다.
+        // x:n = l:100 x = n * l / 100
+
         AsciiImage {
-            width: 0,
-            height: 0,
-            ascii_image: "".to_owned(),
+            width,
+            height,
+            ascii_image,
         }
     }
 }
---
+++ src/bin/ascii_test.rs
@@ -0,0 +1,23 @@
+use sdl_isometric::ascii::AsciiImage;
+
+fn main() {
+    let dog_image = image::io::Reader::open("assets/hacker.png")
+        .unwrap()
+        .decode()
+        .unwrap();
+    let ascii_image = AsciiImage::new(&dog_image, 20, 40);
+    dbg!(&ascii_image);
+
+    let width = ascii_image.width;
+    let height = ascii_image.height;
+    let txt_image = ascii_image.ascii_image;
+
+    for y in 0..height {
+        println!(
+            "{}",
+            txt_image
+                .get((y * width) as usize..((y + 1) * width) as usize)
+                .unwrap()
+        );
+    }
+}
