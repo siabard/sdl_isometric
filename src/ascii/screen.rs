23681bc69f91d855571c73013b36eb4cd9f7cf7f
Yeonho Jang <siabard@gmail.com>
2022-12-27 22:36:22 +0900

    한글과 영어 관련해서 스크린 객체에 관련 내용 삽입

--- src/ascii/screen.rs
+++ src/ascii/screen.rs
@@ -7,6 +7,7 @@
 
 use hangul_jaso::*;
 use jaso_sdl2::*;
+use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::render::WindowCanvas;
 use std::collections::HashMap;
 
@@ -23,16 +24,26 @@ pub struct Screen {
     pub width: u32,
     pub height: u32,
     pub cells: Vec<ScreenCell>,
+    pub cell_width: u32,
+    pub cell_height: u32,
 }
 
 impl Screen {
-    pub fn new(width: u32, height: u32) -> Screen {
+    /// width : 스크린의 물리적 가로
+    /// height : 스크린의 물리적 세로
+    /// cell_width : 한 셀의 가로 길이
+    /// cell_height : 한 셀의 세로 길이
+    /// Screen 에서 표현가능한 셀의 갯수 = (width / cell_width ) * (height / cell_height)
+    pub fn new(width: u32, height: u32, cell_width: u32, cell_height: u32) -> Screen {
         let mut cells = vec![];
 
-        for _ in 0..height {
-            for _ in 0..width {
+        let cell_rows = height / cell_height;
+        let cell_cols = width / cell_width;
+
+        for _ in 0..cell_rows {
+            for _ in 0..cell_cols {
                 cells.push(ScreenCell {
-                    cell: ' ',
+                    cell: '\0',
                     fg: (255, 255, 255, 255),
                     bg: (0, 0, 0, 255),
                 });
@@ -42,6 +53,8 @@ impl Screen {
         Screen {
             width,
             height,
+            cell_width,
+            cell_height,
             cells,
         }
     }
@@ -55,7 +68,7 @@ impl Screen {
         fb: Option<(u8, u8, u8, u8)>,
         bg: Option<(u8, u8, u8, u8)>,
     ) {
-        let idx = (y * self.width + x) as usize;
+        let idx = (y * self.cell_width + x) as usize;
 
         let cell = &self.cells[idx];
         self.cells[idx] = ScreenCell {
@@ -74,7 +87,7 @@ impl Screen {
         fg: Option<(u8, u8, u8, u8)>,
         bg: Option<(u8, u8, u8, u8)>,
     ) {
-        let default_idx = (y * self.width + x) as usize;
+        let default_idx = (y * self.cell_width + x) as usize;
         let default_cell = &self.cells[default_idx];
         let default_fg = if let Some(c) = fg { c } else { default_cell.fg };
         let default_bg = if let Some(c) = bg { c } else { default_cell.bg };
@@ -85,7 +98,7 @@ impl Screen {
             let ucs_2_code = utf8_to_ucs2(&c).unwrap();
             let lang = ucs2_language(ucs_2_code);
 
-            let idx = (y * self.width + x_) as usize;
+            let idx = (y * self.cell_width + x_) as usize;
 
             self.cells[idx] = ScreenCell {
                 cell: c,
@@ -100,5 +113,111 @@ impl Screen {
     }
 
     /// 렌더링
-    pub fn render(&self, font: &HashMap<String, Fonts>, canvas: &mut WindowCanvas) {}
+    pub fn render(
+        &self,
+        font_map: &HashMap<Languages, jaso_sdl2::Fonts>,
+        canvas: &mut WindowCanvas,
+    ) {
+        let texture_creator = canvas.texture_creator();
+
+        let mut texture = texture_creator
+            .create_texture_streaming(
+                sdl2::pixels::PixelFormatEnum::BGRA8888,
+                self.width,
+                self.height,
+            )
+            .unwrap();
+
+        canvas
+            .with_texture_canvas(&mut texture, |texture_canvas| {
+                // 배경은 투명색으로 색칠
+                texture_canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
+                texture_canvas.set_draw_color(sdl2::pixels::Color::from((0, 0, 0, 0)));
+                texture_canvas.clear();
+
+                let cell_rows = self.height / self.cell_height;
+                let cell_cols = self.width / self.cell_width;
+
+                // 전체 셀에 대해 반복
+                // TODO 이 부분은 memoization 등의 최적화를 통해
+                // 속도를 늘릴 수 있을 것으로 기대함.
+                for y in 0..cell_rows {
+                    for x in 0..cell_cols {
+                        let idx: usize = (y * cell_cols + x) as usize;
+                        let cell: &ScreenCell = &self.cells[idx];
+
+                        if cell.cell != '\0' {
+                            let language = hangul_jaso::ucs2_language(
+                                hangul_jaso::utf8_to_ucs2(&cell.cell).unwrap(),
+                            );
+
+                            match language {
+                                Languages::Ascii => {
+                                    if let Some(jaso_sdl2::Fonts::Ascii(font)) =
+                                        font_map.get(&language)
+                                    {
+                                        for j in 0..16_i16 {
+                                            let row = font.fonts[cell.cell as usize][j as usize];
+                                            for i in 0..8_i16 {
+                                                let v = (row << i) & 0x80;
+                                                if v > 0 {
+                                                    texture_canvas.pixel(i, j, cell.fg).unwrap();
+                                                } else {
+                                                    texture_canvas.pixel(i, j, cell.bg).unwrap();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                Languages::Hangul => {
+                                    if let Some(jaso_sdl2::Fonts::Korean(font)) =
+                                        font_map.get(&language)
+                                    {
+                                        let (jaso, bul) = hangul_jaso::build_jaso_bul(&cell.cell);
+
+                                        let cho_hex =
+                                            &font.cho[(jaso.cho + bul.cho.unwrap() * 19) as usize];
+                                        let mid_hex =
+                                            &font.mid[(jaso.mid + bul.mid.unwrap() * 21) as usize];
+                                        let jong_hex = match bul.jong {
+                                            Some(jong) => {
+                                                &font.jong[(jaso.jong + jong * 28) as usize]
+                                            }
+                                            _ => &font.jong[0],
+                                        };
+
+                                        for j in 0..16_i16 {
+                                            let cho = cho_hex[j as usize];
+                                            let mid = mid_hex[j as usize];
+                                            let jong = jong_hex[j as usize];
+                                            for i in 0..16_i16 {
+                                                let vc = (cho << i) & 0x8000;
+                                                let vm = (mid << i) & 0x8000;
+                                                let vj = (jong << i) & 0x8000;
+
+                                                if vc > 0 {
+                                                    texture_canvas.pixel(i, j, cell.fg).unwrap();
+                                                }
+                                                if vm > 0 {
+                                                    texture_canvas.pixel(i, j, cell.fg).unwrap();
+                                                }
+                                                if vj > 0 {
+                                                    texture_canvas.pixel(i, j, cell.fg).unwrap();
+                                                }
+
+                                                if vc + vm + vj == 0 {
+                                                    texture_canvas.pixel(i, j, cell.bg).unwrap();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+            })
+            .unwrap();
+    }
 }
