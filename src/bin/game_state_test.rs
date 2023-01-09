e1ee8ee247c5ed6706063f8b7132bd4d1cf2663b
Yeonho Jang <siabard@gmail.com>
2023-01-09 22:47:11 +0900

    패키지 명 조정

--- src/bin/game_state_test.rs
+++ src/bin/game_state_test.rs
@@ -1,12 +1,13 @@
-use sdl_isometric::ascii::GameState;
+use sdl_isometric::ascii::game_state::GameState;
+use sdl_isometric::ascii::Tile;
 
 fn main() {
     let mut state = GameState::new();
 
-    state.add_entity(Some((1, 1)), Some(sdl_isometric::ascii::Tile::Wall));
-    state.add_entity(Some((2, 3)), Some(sdl_isometric::ascii::Tile::Ascii('c')));
-    state.add_entity(None, Some(sdl_isometric::ascii::Tile::Ascii('d')));
-    state.add_entity(None, Some(sdl_isometric::ascii::Tile::Player));
+    state.add_entity(Some((1, 1)), Some(Tile::Wall));
+    state.add_entity(Some((2, 3)), Some(Tile::Ascii('c')));
+    state.add_entity(None, Some(Tile::Ascii('d')));
+    state.add_entity(None, Some(Tile::Player));
 
     let entities = state.entity_coord_and_tile();
 
